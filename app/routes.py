fc1f0dd703a0d68e15dd97c05f85795ad6e2c4ff
Stefan van der Duijs <stefanvanderduijs@icloud.com>
2024-04-25 20:11:48 +0200

    Added basic pages

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route("/")
 def home():
-    return "<h1>Hello World!</h1>"
+    return render_template("index.html")
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/search/")
+def search():
+    return render_template("search.html")
